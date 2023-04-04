3cb9ae3f98e325bb7988475d0f6557c3c13ca68a
dusorensen <eduardorafaelsorensen@gmail.com>
2023-04-03 21:55:33 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,53 @@
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+const dolar = 5.06
+const euro = 5.52
+
+const convertValues = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currancyValueText = document.getElementById('currency-value-text')
+
+    const sumConvertDolar = (inputReais / dolar).toFixed(2)
+    const sumConvertEuro = (inputReais / euro).toFixed(2)
+
+    realValueText.innerHTML = inputReais
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReais)
+
+    if (select.value === "US$ Dolar Americano") {
+        currancyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(sumConvertDolar)
+    }
+
+    if (select.value === "€ Euro") {
+        currancyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(sumConvertEuro)
+    }
+
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/euro.png"
+    }
+    if (select.value === 'US$ Dolar Americano') {
+        currencyName.innerHTML = "Dolar"
+        currencyImg.src = "./assets/eua.png"
+    }
+
+    select.addEventListener('change', convertValues)
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
+
